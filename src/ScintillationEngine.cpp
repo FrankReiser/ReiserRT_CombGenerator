5ec0b77ef7062a5ea9a5b1c3d4af3812f0efcb9e
Frank Reiser <frank@thereisers.com>
2022-09-23 16:45:58 -0400

    Scintillation Engine Deserves its Own Files
    This needed to be reusable at the next higher level of signal generation so it can be used to scintillate the skin-line.

--- src/ScintillationEngine.cpp
+++ src/ScintillationEngine.cpp
@@ -1,18 +1,18 @@
 /**
- * @file CombGeneratorPrivate.cpp
- * @brief The implementation file for the Comb Generator Private Purposes.
+ * @file ScintillationEngine.cpp
+ * @brief The implementation file for the Scintillation Engine.
  * @authors Frank Reiser
- * @date Initiated August 22nd, 2022
+ * @date Initiated September 23rd, 2022
  */
 
-#include "CombGeneratorPrivate.h"
+#include "ScintillationEngine.h"
 
-#include <random>
+using namespace TSG_NG;
 
-void ScintillationEngine::run( const RandomValueFunkType & randomFunk,
-      ScintillationParamsType & scintillationParams,
-      size_t sampleCounter,
-      size_t decorrelationSamples )
+void ScintillationEngine::run(const RandomValueFunkType & randomFunk,
+                              StateType & scintillationState,
+                              size_t sampleCounter,
+                              size_t decorrelationSamples )
 {
     // Compute Scintillated Magnitude over the Buffer Length.
     auto pScintillationMag = pScintillationBuffer;
@@ -22,7 +22,7 @@ void ScintillationEngine::run( const RandomValueFunkType & randomFunk,
         // This is the current magnitude value (first) plus the change in magnitude per sample (second).
         // We conveniently update the magnitude while we are at it for the next iteration.
         // On the initial sample of zero, the change in magnitude per sample is zero.
-        *pScintillationMag++ = scintillationParams.first += scintillationParams.second;
+        *pScintillationMag++ = scintillationState.first += scintillationState.second;
 
         // If time to calculate a new scintillation slope
         if ( 0 == (sampleCounter++ % decorrelationSamples ) )
@@ -31,7 +31,7 @@ void ScintillationEngine::run( const RandomValueFunkType & randomFunk,
             auto scintillationTargetMag = randomFunk();
 
             // Calculate the change in magnitude per sample and store as the second parameter for the line.
-            scintillationParams.second = ( scintillationTargetMag - scintillationParams.first ) / decorrelationSamples;
+            scintillationState.second = (scintillationTargetMag - scintillationState.first ) / decorrelationSamples;
         }
     }
 }
